8f423958964a778bcbd59abf671873ebc0221aed
Ebubechukwu Idam <68309549+preciousidam@users.noreply.github.com>
2025-07-28 18:55:03 +0100

    updated project to use enum for purpose and compute status from units

--- app/models/project.py
+++ app/models/project.py
@@ -1,5 +1,6 @@
+import enum
 from sqlmodel import SQLModel, Field, Relationship
-from typing import Optional, TYPE_CHECKING
+from typing import Literal, Optional, TYPE_CHECKING
 from datetime import datetime,timezone
 from uuid import uuid4, UUID
 from app.models.timestamp_mixin import TimestampMixin
@@ -7,15 +8,37 @@ from app.models.timestamp_mixin import TimestampMixin
 if TYPE_CHECKING:
     from app.models.unit import Unit
 
+class ProjectPurpose(str, enum.Enum):
+    RESIDENTIAL = "residential"
+    COMMERCIAL = "commercial"
+    MIXED_USE = "mixed_use"
+    INDUSTRIAL = "industrial"
+    OTHER = "other"
+
+class Status(str, enum.Enum):
+    COMPLETED = "completed"
+    ARCHIVED = "archived"
+    ONGOING = "ongoing"
+
 class Project(SQLModel, TimestampMixin, table=True):
     id: UUID = Field(default_factory=uuid4, primary_key=True)
     name: str = Field(index=True)
     description: Optional[str] = None
     address: str
     num_units: int
-    purpose: Optional[str] = None
+    purpose: Optional[ProjectPurpose] = None
     artwork_url: Optional[str] = None
     deleted: bool = False
     reason_for_delete: Optional[str] = None
 
     units: list["Unit"] = Relationship(back_populates="project")
+
+    @property
+    def status(self) -> str:
+        if self.deleted:
+            return Status.ARCHIVED.value
+        if self.units:
+            for unit in self.units:
+                if unit.handover_date == None:
+                    return Status.ONGOING.value
+        return Status.COMPLETED.value
--- app/schemas/project.py
+++ app/schemas/project.py
@@ -1,29 +1,42 @@
+import enum
 from pydantic import BaseModel
 from typing import Optional
 from datetime import datetime
 from uuid import UUID
+
+class ProjectPurpose(str, enum.Enum):
+    RESIDENTIAL = "residential"
+    COMMERCIAL = "commercial"
+    MIXED_USE = "mixed_use"
+    INDUSTRIAL = "industrial"
+    OTHER = "other"
 
 class ProjectCreate(BaseModel):
     name: str
     description: Optional[str] = None
     address: str
     num_units: int
-    purpose: Optional[str] = None
+    purpose: Optional[ProjectPurpose] = None
     artwork_url: Optional[str] = None
 
 class ProjectRead(ProjectCreate):
     id: UUID
     deleted: bool = False
     reason_for_delete: Optional[str] = None
+    status: str | None = None  # This will be derived from the model logic
     created_at: datetime
     updated_at: datetime
+
+    model_config = {
+        "from_attributes": True
+    }
 
 class ProjectUpdate(BaseModel):
     name: Optional[str] = None
     description: Optional[str] = None
     address: Optional[str] = None
     num_units: Optional[int] = None
-    purpose: Optional[str] = None
+    purpose: Optional[ProjectPurpose] = None
     artwork_url: Optional[str] = None
 
 class ProjectReplace(BaseModel):
@@ -31,7 +44,7 @@ class ProjectReplace(BaseModel):
     description: Optional[str] = None
     address: str
     num_units: int
-    purpose: Optional[str] = None
+    purpose: Optional[ProjectPurpose] = None
     artwork_url: Optional[str] = None
 
 class ProjectList(BaseModel):
