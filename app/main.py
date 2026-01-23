1527e83e515441caa26e8bb61f8c49dd9e2d336a
Ebubechukwu Idam <68309549+preciousidam@users.noreply.github.com>
2026-01-23 16:12:43 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -9,6 +9,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.ALLOWED_ORIGINS,
+    allow_origin_regex=r"^http://localhost(:\d+)?$|^http://127\.0\.0\.1(:\d+)?$",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
